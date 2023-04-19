5e154ae7359f0d58a04cf2530c3e7a6dcecbf584
mtk2201 <mtk2201@uw.edu>
2023-04-18 18:03:47 -0700

    changed aboutpage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import "../AboutPage.css";
 
+
+// always pull, edit, commit, pull again, and then push
 export default function AboutPage(props) {
-
     return (
-        <div>
-            About Page
+        <div class="parent">
+            <img
+                src="./pics/tomato.png"
+                alt="tomato"
+                className="tomato"
+            />
+            <img
+                src="./pics/greenEllipse5.png"
+                alt="greenEllipse"
+                className="greenEllipse"
+            />
+            <p className="familyHistory">Family History</p>
+            <div className="greenRectangle"></div>
+            <img 
+                src="./pics/2people.png"
+                alt="2people"
+                className="people"
+            />
+            <div className="greenBackground"></div>
+            <div className="smallGreenCircle"></div>
+            <div className="smallOrangeCircle"></div>
+            <div className="lowBlueCircle"></div>
+            <div className="bigOrangeCircle"></div>
+            <div className="orangeCircle"></div>
+            <img 
+                src="./pics/egg.png"
+                alt="egg"
+                className="egg"
+            />
+            <img 
+                src="./pics/girl.png" 
+                alt="girl"
+                className="girl"
+            />
+            <div className="ellipse"></div>
+            <div className="Rectangle"></div>
         </div>
     )
 }
