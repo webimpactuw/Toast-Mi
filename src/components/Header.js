0043f15c0b884364ee7d46deda746f145e1f1849
Justin Tham <justinctham@hotmail.com>
2023-04-19 18:00:02 -0700

    restructured bootstrap import

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
     const navList = ["About", "Menu", "Order"];
