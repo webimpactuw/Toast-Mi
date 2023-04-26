d5b7641fdf27038013de4792fe84540752285540
juliette <jiyjiy23@gmail.com>
2023-04-25 18:51:02 -0700

    orderpage almost done

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -22,7 +22,7 @@ export default function OrderPage(props) {
 
             <div class = "greenBox">
                 <div class="images">
-                    {/* <p> test</p> */}
+
                     <div className='row'>
                         <div className="col">
                             {/* <img src="pics/TacomaOrder.png"></img> */}
@@ -30,15 +30,108 @@ export default function OrderPage(props) {
                                 <h3>Tacoma</h3>
                                 <p>2602 N Proctor St Suite D <br/> Tacoma, WA 98407</p>
                                 <p>(253)-245-2246</p>
-                                <p className="dotRow">..............................................................</p>
+                                {/* <br/> */}
+                                <hr className="dottedLine"></hr>
                                 <p> Order at:</p>
-                                <p>..............................................................</p>
+                                <hr className="dottedLine"></hr>
+
+                                {/* Receipt Effect */}
+                                <div className='row'>
+                                    <div className='col'>
+                                        <div className='websiteNames'>
+                                            <p><a href='url'>Doordash</a></p>
+                                            <p><a href='url'>Ubereats</a></p>
+                                            <p><a href='url'>Grubhub</a></p>
+                                            <p><a href='url'>Clover</a></p>
+                                        </div>
+                                    </div>
+
+                                    <div className='col'>
+                                        <div className='prices'>
+                                            <p>0.00</p>
+                                            <p>0.00</p>
+                                            <p>0.00</p>
+                                            <p>0.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="dottedLine"></hr>
+                                <div className='row'>
+                                    <div className='col'>
+                                        <div className='websiteNames'>
+                                           <p>Total:</p> 
+                                        </div>
+                                    </div>
+                                    <div className='col'>
+                                        <div className='deliciousness'>
+                                            <p>Deliciousness!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="dottedLine"></hr>
+                                {/* Receipt Effect */}
+
+                                <p>Thank  you  for  visiting!</p>
+                                <a href='https://www.google.com/maps/place/Toast+Mi+Tacoma/@47.2713449,-122.4918014,17z/data=!3m2!4b1!5s0x549054ef2265017d:0x1cc5da800182baa4!4m6!3m5!1s0x5490555380efde05:0x9285e2e7a142ae!8m2!3d47.2713449!4d-122.4892265!16s%2Fg%2F11hf9slg_3' target='_blank'>
+                                    <img src="pics/tacomaMap.png"></img>
+                                </a>
+                                <img src="pics/logo.jpg" className='bottomLogo'></img>
                             </div>
                         </div>
                         <div className="col">
                             {/* <img src="pics/GreenlakeOrder.png" alt="GreenlakeOrder"></img> */}
                             <div className="innerWhiteBox" id="secondBox">
                                 <h3>GreenLake</h3>
+                                <p>7130 Woodlawn Ave NE <br/> Seattle, WA 98115</p>
+                                <p>(206)-402-5546</p>
+                                {/* <br/> */}
+                                <hr className="dottedLine"></hr>
+                                <p> Order at:</p>
+                                <hr className="dottedLine"></hr>
+
+                                {/* Receipt Effect */}
+                                <div className='row'>
+                                    <div className='col'>
+                                        <div className='websiteNames'>
+                                            <p><a href='url'>Doordash</a></p>
+                                            <p><a href='url'>Ubereats</a></p>
+                                            <p><a href='url'>Grubhub</a></p>
+                                            <p><a href='url'>Clover</a></p>
+                                        </div>
+                                    </div>
+
+                                    <div className='col'>
+                                        <div className='prices'>
+                                            <p>0.00</p>
+                                            <p>0.00</p>
+                                            <p>0.00</p>
+                                            <p>0.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="dottedLine"></hr>
+
+
+                                <div className='row'>
+                                    <div className='col'>
+                                        <div className='websiteNames'>
+                                           <p>Total:</p> 
+                                        </div>
+                                    </div>
+                                    <div className='col'>
+                                        <div className='deliciousness'>
+                                            <p>Deliciousness!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="dottedLine"></hr>
+                                {/* Receipt Effect */}
+
+                                <p>Thank  you  for  visiting!</p>
+                                <a href='https://www.google.com/maps/place/Toast+Mi+Green+Lake/@47.6802528,-122.3270411,17z/data=!3m1!4b1!4m6!3m5!1s0x549015db737bb9c7:0x88904a12f4e8e731!8m2!3d47.6802528!4d-122.3244662!16s%2Fg%2F11t5q7xhj9' target='_blank'>
+                                   <img src="pics/greenlakeMap.png"></img> 
+                                </a>
+                                <img src="pics/logo.jpg" className='bottomLogo'></img>
                                 
                             </div>
                         </div>
