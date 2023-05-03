f56e782706216f6ca8dbbc10cb8572785859ca7d
Justin Tham <justinctham@hotmail.com>
2023-05-03 16:09:09 -0700

    banh mi card

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Menu(props) {
+function Menu(props) {
 
     return (
         <div className='container'>
@@ -59,6 +59,25 @@ export default function Menu(props) {
     )
 }
 
-function MenuPage(props) {
-    
+export default function MenuPage(props) {
+    return (
+        <div className='header-box d-flex flex-column justify-content-center'>
+            <h1 className='menu-header'>Banh Mi</h1>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        {/* <img src="..." class="img-fluid rounded-start" alt="..."/> */}
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
 }
