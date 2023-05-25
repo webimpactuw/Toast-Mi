224b2774f6a597b86dcacfe073bd94fe7f3d6711
Joshua Tan <31526067+joshtan2@users.noreply.github.com>
2023-05-24 17:40:21 -0700

    Added sanity to order page

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../App.css";
+import sanityClient from '../client'
+import imageUrlBuilder from '@sanity/image-url';
 
+const builder = imageUrlBuilder(sanityClient);
+
+function urlFor(source) {
+    return builder.image(source);
+}
 export default function OrderPage(props) {
+    const [logo, setlogo] = useState(
+        'image-45c8e6269aa3b3cd3f5e73ba8e04f470fc794c8c-266x146-png');
+    useEffect(() => {
+        sanityClient.fetch('*[_type == "logo"]').then((data) => {
+            setlogo(data[0].logo.asset._ref)
+        }).catch(error => console.log(error))
 
+    }, []);
     return (
-        
+
         <div>
-             <div class="header">
+            <div class="header">
                 <h1>Order</h1>
                 <p>Pickup and Delivery</p>
             </div>
 
-            <div class = "content">
+            <div class="content">
                 <p>----------- open daily from 11am to 6pm -----------</p>
             </div>
 
-            <div class = "greenBox">
+            <div class="greenBox">
                 <div class="images">
 
                     <div className='row'>
@@ -23,7 +37,7 @@ export default function OrderPage(props) {
                             {/* <img src="pics/TacomaOrder.png"></img> */}
                             <div className="innerWhiteBox" id="order-firstBox">
                                 <h3>Tacoma</h3>
-                                <p>2602 N Proctor St Suite D <br/> Tacoma, WA 98407</p>
+                                <p>2602 N Proctor St Suite D <br /> Tacoma, WA 98407</p>
                                 <p>(253)-245-2246</p>
                                 {/* <br/> */}
                                 <hr className="dottedLine"></hr>
@@ -54,7 +68,7 @@ export default function OrderPage(props) {
                                 <div className='row'>
                                     <div className='col'>
                                         <div className='websiteNames'>
-                                           <p>Total:</p> 
+                                            <p>Total:</p>
                                         </div>
                                     </div>
                                     <div className='col'>
@@ -71,14 +85,14 @@ export default function OrderPage(props) {
                                     <img src="pics/tacomaMap.png"></img>
                                 </a> */}
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2707.1999933594566!2d-122.4892265!3d47.271344899999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5490555380efde05%3A0x9285e2e7a142ae!2sToast%20Mi%20Tacoma!5e0!3m2!1sen!2sus!4v1683159430837!5m2!1sen!2sus" width="400" height="300" styles="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                                <img src="pics/logo.jpg" className='bottomLogo'></img>
+                                <img src={urlFor(logo)} className='bottomLogo'></img>
                             </div>
                         </div>
                         <div className="col">
                             {/* <img src="pics/GreenlakeOrder.png" alt="GreenlakeOrder"></img> */}
                             <div className="innerWhiteBox" id="order-secondBox">
                                 <h3>GreenLake</h3>
-                                <p>7130 Woodlawn Ave NE <br/> Seattle, WA 98115</p>
+                                <p>7130 Woodlawn Ave NE <br /> Seattle, WA 98115</p>
                                 <p>(206)-402-5546</p>
                                 {/* <br/> */}
                                 <hr className="dottedLine"></hr>
@@ -111,7 +125,7 @@ export default function OrderPage(props) {
                                 <div className='row'>
                                     <div className='col'>
                                         <div className='websiteNames'>
-                                           <p>Total:</p> 
+                                            <p>Total:</p>
                                         </div>
                                     </div>
                                     <div className='col'>
@@ -129,16 +143,16 @@ export default function OrderPage(props) {
                                 </a> */}
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2686.2145689734325!2d-122.32704112289025!3d47.68025277119679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549015db737bb9c7%3A0x88904a12f4e8e731!2sToast%20Mi%20Green%20Lake!5e0!3m2!1sen!2sus!4v1683160138237!5m2!1sen!2sus" width="400" height="300" styles="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 {/* <img className="eggie" src="pics/egg.png" alt="egg"></img> */}
-                                <img src="pics/logo.jpg" className='bottomLogo'></img>
+                                <img src={urlFor(logo)} className='bottomLogo'></img>
                                 {/* <img className="eggie" src="pics/egg.png" alt="egg"></img> */}
-                                
+
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
-                
+
             </div>
-        </div>       
+        </div>
     )
 }
